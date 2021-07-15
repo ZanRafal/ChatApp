7db921083d395a616b84f020875b26e145210a3c
ZanRafal <01159225@pw.edu.pl>
2021-07-16 01:15:27 +0200

    Added primary logic to Server class

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,32 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     public static void main(String[] args) {
+        ConsoleHelper.writeMessage("Input server port:");
+        int port = ConsoleHelper.readInt();
 
+        try (ServerSocket serverSocket = new ServerSocket(port)){
+            ConsoleHelper.writeMessage("The server is currently running...");
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new Handler(socket).start();
+            }
+        } catch (Exception e) {
+            ConsoleHelper.writeMessage("An error has occurred while starting or running the server..");
+        }
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+        }
     }
 }
